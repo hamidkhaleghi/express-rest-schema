4c5cc51f9c4e0f3793ac9e42c812782f90b56dfc
Hamid Khaleghi <hamidxkhaleghi@gmail.com>
2020-09-21 12:08:11 +0330

    #

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const response = (
 	res,
 	{ response = null, message = null, error = null, status = 200 } = {}
 ) => {
+    if (!res) throw new Error('set the response object')
 	return res.status(status).send({
 		status,
 		result: response,
